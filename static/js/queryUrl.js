c0757d38c60e15d7a8f2cac0d1a77ea5b7f26273
Tomeka Morrison <tomekamorrison@Tomekas-MacBook-Pro.local>
2019-10-05 10:44:57 -0500

    Updated app.js, style.css and index.html

--- static/js/queryUrl.js
+++ static/js/queryUrl.js
@@ -1,16 +1,14 @@
-
-d3.csv("/data/alt_fuel.csv",function(data) {
-    console.log(data);
-    createFeatures(data.stations);
-  });
-
-//algoria code
-
-
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=200&api_key=ApaTmgR89mLvljGxv0g3TINHNSrc6zMUDR5LxK99&format=JSON";
+// Perform a GET request to the query URL
+d3.json(queryUrl, function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.fuel_stations);
+});
 function placeholder() {
     var placesAutocomplete = places({
-        appId: 'plBKUX3N3ZPX',
-        apiKey: 'f18f918b72eaf29b158f4430a5b280d1',
+        appId: 'pl0DEKJ4XSAE',
+        apiKey: '9fe8de3ec445c3f66f52197966c2e68f',
         container: document.querySelector('#input-map')
     });
     var map = L.map('map-example-container', {
@@ -83,4 +81,3 @@ function placeholder() {
     }
 }
 placeholder();
-
