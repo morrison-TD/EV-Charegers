a5b4876e74530a6dc9f638a140d4421234375f21
morrison-TD <49292864+morrison-TD@users.noreply.github.com>
2019-12-23 11:23:51 -0600

    Revert "Update HTML"
    Revert HTML changes
    
    :wq
    This reverts commit 366bd6ac67b4588993a03a4bb109f7a52bf15bdc.
    :wq

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // all custom jQuery will go here
+    // all custom jQuery will go heress
 $('#EV-Table').DataTable();
 $('.dataTables_length').addClass('bs-select');
 });
